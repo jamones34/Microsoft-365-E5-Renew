fad59b55222902c433023f5f9e21fa30a355cd39
jamones34 <104509247+jamones34@users.noreply.github.com>
2022-04-27 17:55:12 +0200

    Update UpdateToken.py

--- UpdateToken.py
+++ UpdateToken.py
@@ -18,21 +18,21 @@ geturl=r'https://api.github.com/repos/'+gh_repo+r'/actions/secrets/public-key'
 #puturl=r'https://api.github.com/repos/'+gh_repo+r'/actions/secrets/MS_TOKEN'
 key_id='wangziyingwen'
 
-#公钥获取
+#Get public key
 def getpublickey(Auth,geturl):
     headers={'Accept': 'application/vnd.github.v3+json','Authorization': Auth}
     html = req.get(geturl,headers=headers)
     jsontxt = json.loads(html.text)
     if 'key' in jsontxt:
-        print("公钥获取成功")
+        print("Public key obtained successfully")
     else:
-        print("公钥获取失败，请检查secret里 GH_TOKEN 格式与设置是否正确")
+        print("Failed to obtain the public key, please check whether the GH_TOKEN format and settings in the secret are correct")
     public_key = jsontxt['key']
     global key_id 
     key_id = jsontxt['key_id']
     return public_key
 
-#微软refresh_token获取
+#Microsoft refresh_token acquisition
 def getmstoken(ms_token,appnum):
     headers={'Content-Type':'application/x-www-form-urlencoded'
             }
@@ -45,34 +45,34 @@ def getmstoken(ms_token,appnum):
     html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
     jsontxt = json.loads(html.text)
     if 'refresh_token' in jsontxt:
-        print(r'账号/应用 '+str(appnum)+' 的微软密钥获取成功')
+        print(r'Account/App '+str(appnum)+' The Microsoft key was obtained successfully')
     else:
-        print(r'账号/应用 '+str(appnum)+' 的微软密钥获取失败'+'\n'+'请检查secret里 CLIENT_ID , CLIENT_SECRET , MS_TOKEN 格式与内容是否正确，然后重新设置')
+        print(r'Account/App '+str(appnum)+' The Microsoft key acquisition failed'+'\n'+'Please check the secret CLIENT_ID , CLIENT_SECRET , MS_TOKEN Is the format and content correct, then reset')
     refresh_token = jsontxt['refresh_token']
     access_token = jsontxt['access_token']
     return refresh_token
-#是否要保存access，以降低微软token刷新率???
+#Do you want to save access，To reduce the refresh rate of Microsoft token???
 
-#token加密
+#token encryption
 def createsecret(public_key,secret_value):
     public_key = public.PublicKey(public_key.encode("utf-8"), encoding.Base64Encoder())
     sealed_box = public.SealedBox(public_key)
     encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
     return b64encode(encrypted).decode("utf-8")
 
-#token上传
+#token upload
 def setsecret(encrypted_value,key_id,puturl,appnum):
     headers={'Accept': 'application/vnd.github.v3+json','Authorization': Auth}
     #data={'encrypted_value': encrypted_value,'key_id': key_id}  ->400error
     data_str=r'{"encrypted_value":"'+encrypted_value+r'",'+r'"key_id":"'+key_id+r'"}'
     putstatus=req.put(puturl,headers=headers,data=data_str)
     if putstatus.status_code >= 300:
-        print(r'账号/应用 '+str(appnum)+' 的微软密钥上传失败，请检查secret里 GH_TOKEN 格式与设置是否正确')
+        print(r'Account/App '+str(appnum)+' Microsoft key upload failed，Please check if the GH_TOKEN format and settings in the secret are correct')
     else:
-        print(r'账号/应用 '+str(appnum)+' 的微软密钥上传成功')
+        print(r'Account/App '+str(appnum)+' The Microsoft key was uploaded successfully')
     return putstatus
     
-#调用 
+#transfer 
 for a in range(1, int(app_num)+1):
     client_id=os.getenv('CLIENT_ID_'+str(a))
     client_secret=os.getenv('CLIENT_SECRET_'+str(a))
